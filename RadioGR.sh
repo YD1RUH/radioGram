9690c6a88c2f6ce8bbc4e2cecb6ae79e47e1cf1e
YD1RUH <32731955+YD1RUH@users.noreply.github.com>
2022-01-23 09:38:56 +0700

    Update RadioGR.sh

--- RadioGR.sh
+++ RadioGR.sh
@@ -1,24 +1,24 @@
 clear
-
-read -p "masukkan modulasi     : " modulasi
-read -p "pesan dari            : " dari
-read -p "untuk                 : " ke
 echo ""
-echo "kategori berita [1] biasa, [2] darurat"
-read -p "pilih kategori berita : " kategori
+read -p "modulation type, ex:200 : " modulasi
+read -p "message from            : " dari
+read -p "to                      : " ke
+echo ""
+echo "message category [1] usual, [2] emergency"
+read -p "choose category use : " kategori
 if [ "$kategori" = "1" ]; then
-    kategori_fix="biasa"
+    kategori_fix="usual"
 elif [ "$kategori" = "2" ]; then
-    kategori_fix="darurat"
+    kategori_fix="emergency"
 else
     clear
     echo "."
     sleep 1
-    echo "pilihan anda tidak ada dalam program.."
+    echo "your choice not listed on programm.."
     sleep 1
     echo "."
     sleep 1
-    echo "mengulang program.."
+    echo "restart program.."
     sleep 2
     bash RadioGR.sh
 fi
@@ -26,18 +26,18 @@ nomor=0
 while true
 do
 clear
-echo "callsign from "$dari" to "$ke" kategori "$kategori_fix", with modulation : "$modulasi
-echo "masukkan isi pesan : ";read pesan
+echo "callsign from "$dari" to "$ke" category "$kategori_fix", with modulation : "$modulasi
+echo "input your message: ";read pesan
 waktu=$(date)
 nomor=$(($nomor+1))
 echo "..........................................." > radiogram
 echo "..........................................." >> radiogram
 echo "..........................................." >> radiogram
 echo "">> radiogram
-echo "Dari         : $dari" >> radiogram
-echo "Kepada       : $ke" >> radiogram
-echo "Klasifikasi  : $kategori_fix" >> radiogram
-echo "Waktu        : $waktu" >> radiogram
+echo "From           : $dari" >> radiogram
+echo "To             : $ke" >> radiogram
+echo "Classification : $kategori_fix" >> radiogram
+echo "Time           : $waktu" >> radiogram
 echo "" >> radiogram
 echo "Isi Berita   : " >> radiogram
 echo "$pesan" >> radiogram
